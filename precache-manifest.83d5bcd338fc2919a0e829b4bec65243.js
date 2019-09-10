a97228cbf30599f312f285a7dee6114e06e9af80
VAO61 <vao.frontend@gmail.com>
2019-09-10 14:14:38 +0300

    2.1

---
+++ precache-manifest.83d5bcd338fc2919a0e829b4bec65243.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9608728182b750ff5401bd1006cbac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70da11013e645d6142f1",
+    "url": "/static/css/main.032b604b.chunk.css"
+  },
+  {
+    "revision": "2957ecca2d1445fde274",
+    "url": "/static/js/2.2d0ae526.chunk.js"
+  },
+  {
+    "revision": "70da11013e645d6142f1",
+    "url": "/static/js/main.aafb97fb.chunk.js"
+  },
+  {
+    "revision": "7558a35bb3217579cca8",
+    "url": "/static/js/runtime~main.519c26d6.js"
+  },
+  {
+    "revision": "45cf79717f81643efc2249f39cdfca5d",
+    "url": "/static/media/github-logo.45cf7971.svg"
+  },
+  {
+    "revision": "96cc10c248bbd43c2532335f2aa1e8dd",
+    "url": "/static/media/github-search-logo.96cc10c2.svg"
+  },
+  {
+    "revision": "866be30cb2afd99e076058c1934ceefd",
+    "url": "/static/media/icon-list.866be30c.svg"
+  },
+  {
+    "revision": "4718135fd3fc5fa1757ec7289bc89137",
+    "url": "/static/media/icon-star.4718135f.svg"
+  },
+  {
+    "revision": "4e2f984bdbcf6801b5d699a46ddceab7",
+    "url": "/static/media/icon-tile.4e2f984b.svg"
+  }
+]);
